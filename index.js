5a2a482fb481b52faab3bafe7c047ee6fe6fa285
Ted <u332799@gmail.com>
2018-10-03 16:42:44 +0800

    a

--- index.js
+++ index.js
@@ -1,42 +1,76 @@
-var linebot = require('linebot');
-var express = require('express');
+var linebot = require("linebot");
+var express = require("express");
 
 var bot = linebot({
-  channelId: '1611707234',
-  channelSecret: 'ed22fd81fb73114c0e6484ab372b5466',
-  channelAccessToken: '6We1ZNjErd3nG++Rj3UbXHC6Lr30PqTo8a7i1OR+hWf7xqfu/OLpm2ghz5TpSxMjzVQ1qfM33FsxMP/6oP7lST6KhtgCQFSbZWnwnL/zTFoeuprpCgFPDRHo6fehA463oXWmyb8EwhK9MG8x2UTA7wdB04t89/1O/w1cDnyilFU='
+  channelId: "1611707234",
+  channelSecret: "ed22fd81fb73114c0e6484ab372b5466",
+  channelAccessToken:
+    "6We1ZNjErd3nG++Rj3UbXHC6Lr30PqTo8a7i1OR+hWf7xqfu/OLpm2ghz5TpSxMjzVQ1qfM33FsxMP/6oP7lST6KhtgCQFSbZWnwnL/zTFoeuprpCgFPDRHo6fehA463oXWmyb8EwhK9MG8x2UTA7wdB04t89/1O/w1cDnyilFU="
 });
 
 //這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
-bot.on('message', function(event) {
-  if (event.message.type = 'text') {
-    var userId = event.source.userId;
+bot.on("message", function(event) {
+  var userId = event.source.userId;
     var msg = event.message.text;
-  //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply("林都市條豬__").then(function(data) {
-      // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
-      console.log("UserId = " +  userId );
-      //console.log("林都市條豬");
-    }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
-      console.log('錯誤產生，錯誤碼：'+error);
-    });
+  if ((event.message.type = "text")) 
+  {
+    switch (event.message.text) {
+        case 'A':
+            event.source.profile().then(function (profile) {
+            return event.reply('AAAAAA ' + profile.displayName + ' ' + profile.userId);
+        });
+        /*
+            let data;
+            rp(aqiOpt)
+            .then(function (repos) {
+                data = readAQI(repos);
+                event.reply(data.County + data.SiteName +
+                '\n\nPM2.5指數：'+ data["PM2.5_AVG"] + 
+                '\n狀態：' + data.Status);
+            })
+            .catch(function (err) {
+                event.reply('無法取得空氣品質資料～');
+            });
+            */
+            break;
+
+        case 'B':
+            event.source.profile().then(function (profile) {
+                return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+            });
+            break;
+    }
+    break;
+      /*
+    //收到文字訊息時，直接把收到的訊息傳回去
+    event
+      .reply("林都市條豬__")
+      .then(function(data) {
+        // 傳送訊息成功時，可在此寫程式碼
+        console.log(msg);
+        console.log("UserId = " + userId);
+        //console.log("林都市條豬");
+      })
+      .catch(function(error) {
+        // 傳送訊息失敗時，可在此寫程式碼
+        console.log("錯誤產生，錯誤碼：" + error);
+      });
+      */
   }
 });
 
-setTimeout(function(){
-    var userId = 'U9ffbb6488e43aa1da8084065f3d62cf1';
-    var sendMsg = '要發送的文字';
-    bot.push(userId,sendMsg);
-    console.log('send: '+sendMsg);
-},5000);
+setTimeout(function() {
+  var userId = "U9ffbb6488e43aa1da8084065f3d62cf1";
+  var sendMsg = "要發送的文字";
+  bot.push(userId, sendMsg);
+  console.log("send: " + sendMsg);
+}, 5000);
 
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post("/", linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
-  console.log('目前的port是__', port);
+  console.log("目前的port是__", port);
 });
