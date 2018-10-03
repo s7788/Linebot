17ea91a1def6df54c6e29a1466a7b92dc0493719
s7788 <u332799@gmail.com>
2018-10-03 11:58:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ bot.on('message', function(event) {
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
+      //console.log(msg);
+      console.log("林都市條豬");
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
