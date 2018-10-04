7b888b8bfcac4e2d68962a20d38e5e89625185c6
Ted <u332799@gmail.com>
2018-10-04 13:25:17 +0800

    1
    This reverts commit bb9b7600e7f9097db103bd73984c054f9e206938.

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ bot.on("message", function(event) {
   var userId = event.source.userId;
   var msg = event.message.text;
   event.source.profile().then(function(profile) {
-    UserName = profile.displayName;
+    UserName = rofile.displayName;
+    console.log("1");
+    console.log(rofile.displayName);
+    console.log(UserName);
   });
   if ((event.message.type = "text")) {
     switch (event.message.text) {
